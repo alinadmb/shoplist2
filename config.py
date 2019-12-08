9e70d881c07867a2d36a60b87f28ec12cd614c54
alinadmb <43730690+alinadmb@users.noreply.github.com>
2019-12-08 20:22:05 +0300

    hired database_uri

--- config.py
+++ config.py
@@ -7,9 +7,8 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    #                            'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_DATABASE_URI = 'postgres://hasszyqlmsllov:a4b06cdf239afa543a717b10e0bf496c5328dfcdde36d5d942b2f08acf079e23@ec2-174-129-255-39.compute-1.amazonaws.com:5432/dfjbumi1kkkj2u'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 
